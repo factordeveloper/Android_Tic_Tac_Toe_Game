9030ffbf9da3636177f36c7fd9af1027e56db784
Factor Developer <felipeherrerapinzon@gmail.com>
2025-07-09 07:35:49 -0500

    politica de privacidad agregada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.factordev.tic_tac_toe_game"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
